fbaf7372f6d565c33ad9f4d97fad63f149dd008c
Pablo Gorosito <pablo.92pgg@gmail.com>
2023-05-08 01:51:04 +0200

    Se agrega boton agregar al carrito, quitar al carrito, y  para ordenar productos, se agrega libreria externa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // creamos un array de objetos que contiene la informacion de los productos
-const productos = [ 
+let productos = [ 
   {
     id: '1',
     nombre: 'Play Station 4 500gb',
@@ -7,6 +7,7 @@ const productos = [
     stock: 8,
     imagen: 'ps4 slim 500gb.jpg',
     tipo: 'ps4',
+    cantidad: 0,
     esOferta: true,
     cuotasSinInteres: false
   },
@@ -18,6 +19,7 @@ const productos = [
     stock: 5,
     imagen: 'ps4 slim 1tb.jpg',
     tipo: 'ps4',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: true
   },
@@ -29,6 +31,7 @@ const productos = [
     stock: 4,
     imagen: 'dualshock4.jpg',
     tipo: 'ps4',
+    cantidad: 0,
     esOferta: true,
     cuotasSinInteres: false
   },
@@ -40,6 +43,7 @@ const productos = [
     stock: 3,
     imagen: 'crash3.png',
     tipo: 'ps4',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: false
   },
@@ -51,6 +55,7 @@ const productos = [
     stock: 2,
     imagen: 'crash4.png',
     tipo: 'ps4',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: false
   },
@@ -62,6 +67,7 @@ const productos = [
     stock: 3,
     imagen: 'mafia.png',
     tipo: 'ps4',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: true
   },
@@ -73,6 +79,7 @@ const productos = [
     stock: 2,
     imagen: 'MK11.png',
     tipo: 'ps4',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: false
   },
@@ -84,6 +91,7 @@ const productos = [
     stock: 3,
     imagen: 'doom.png',
     tipo: 'ps4',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: true
   },
@@ -95,6 +103,7 @@ const productos = [
     stock: 5,
     imagen: 'ps5 normal.jpg',
     tipo: 'ps5',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: false
   },
@@ -106,6 +115,7 @@ const productos = [
     stock: 3,
     imagen: 'ps5 digital.jpg',
     tipo: 'ps5',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: true
   },
@@ -117,6 +127,7 @@ const productos = [
     stock: 6,
     imagen: 'dualsense ps5.jpg',
     tipo: 'ps5',
+    cantidad: 0,
     esOferta: true,
     cuotasSinInteres: false
   },
@@ -128,6 +139,7 @@ const productos = [
     stock: 2,
     imagen: 'vr ps5.png',
     tipo: 'ps5',
+    cantidad: 0,
     esOferta: true,
     cuotasSinInteres: false
   },
@@ -139,6 +151,7 @@ const productos = [
     stock: 4,
     imagen: 'gta ps5.png',
     tipo: 'ps5',
+    cantidad: 0,
     esOferta: true,
     cuotasSinInteres: false
   },
@@ -150,6 +163,7 @@ const productos = [
     stock: 7,
     imagen: 'sonic ps5.png',
     tipo: 'ps5',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: false
   },
@@ -161,6 +175,7 @@ const productos = [
     stock: 3,
     imagen: 'final fantasy ps5.png',
     tipo: 'ps5',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: false
   },
@@ -172,16 +187,11 @@ const productos = [
     stock: 5,
     imagen: 'gran turismo 7.png',
     tipo: 'ps5',
+    cantidad: 0,
     esOferta: false,
     cuotasSinInteres: true
   },
 ];
-
-// inicializamos las variables para el total y la cantidad de productos que se compran
-const carrito = {
-  total: 0,
-  cantidad: 0,
-};
 
 function generarProductos(tipo, ordenar)
 {
@@ -245,25 +255,167 @@ function generarProductosOferta()
       });
 }
 
-function agregarAlCarrito(id) {
-  let carrito = localStorage.getItem('carrito');
-  if (carrito) {
-      carrito = JSON.parse(carrito);
-      if (carrito[id]) {
-          carrito[id] += 1;
-      } else {
-          carrito[id] = 1;
-      }
-  } else {
-      carrito = {};
-      carrito[id] = 1;
-  }
-  localStorage.setItem('carrito', JSON.stringify(carrito));
-  actualizarCarrito()
+function generarCarrito() {
+  let carrito = obtenerCarrito();
+  // Obtenemos el elemento del DOM donde queremos agregar la tabla
+  const contenedorTabla = document.querySelector('.container-carrito .row');
+
+  // Creamos la tabla y le agregamos los estilos de Bootstrap
+  let tabla = '<table class="table table-centered">';
+
+  // Creamos el encabezado de la tabla con los títulos de las columnas
+  tabla += '<thead><tr>';
+  tabla += '<th>Nombre</th>';
+  tabla += '<th>Imagen</th>';
+  tabla += '<th>Precio</th>';
+  tabla += '<th>Cantidad</th>';
+  tabla += '<th>Subtotal</th>';
+  tabla += '<th></th>';
+  tabla += '</tr></thead>';
+  // Creamos el cuerpo de la tabla con los productos agregados al carrito
+  let cuerpo = '<tbody>';
+  let totalCarrito = 0;
+  let cantidadCarrito = 0;
+
+  carrito.productos.forEach(producto => {
+    if (producto.cantidad && producto.cantidad > 0) {
+      cuerpo += '<tr>';
+
+      cuerpo += `<td>${producto.nombre}</td>`;
+      cuerpo += `<td> <img src="../img/${producto.imagen}" style="width:100px" class="card-img-top producto-zoom" alt="${producto.nombre}"></td>`;
+      cuerpo += `<td>${producto.precio}</td>`;
+      cuerpo += `<td>${producto.cantidad}</td>`;
+
+      const subtotal = producto.cantidad * producto.precio;
+      cuerpo += `<td>${subtotal}</td>`;
+
+      cuerpo += '<td>';
+      cuerpo += `<button class="btn btn-danger" onclick="quitarAlCarrito('${producto.id}'); generarTabla();">Quitar</button>`;
+      cuerpo += '</td>';
+
+      cuerpo += '</tr>';
+
+      totalCarrito += subtotal;
+      cantidadCarrito += producto.cantidad;
+    }
+  });
+  cuerpo += '</tbody>';
+
+  tabla += cuerpo;
+
+  // Creamos el pie de página de la tabla con el total y la cantidad de productos en el carrito
+  let pieDePagina = '<tfoot><tr>';
+  pieDePagina += '<td colspan="2"></td>';
+  pieDePagina += `<td id="cantidad">${cantidadCarrito}</td>`;
+  pieDePagina += `<td id="total">${totalCarrito}</td>`;
+  pieDePagina += '<td></td>';
+  pieDePagina += '</tr></tfoot>';
+
+  tabla += pieDePagina;
+
+  tabla += '</table>';
+
+  contenedorTabla.innerHTML = carrito.productos.length > 0 ? tabla : '<b>No se han agregado productos al carrito.</b>';
 }
 
-function actualizarCarrito() {
-  const carrito = localStorage.getItem('carrito');
-  const cantidadProductosEnCarrito = carrito ? Object.values(JSON.parse(carrito)).reduce((a, b) => a + b) : 0;
-  document.getElementById('cantidad-en-carrito').textContent = cantidadProductosEnCarrito;
+function agregarAlCarrito(idProducto) {
+  let carrito = obtenerCarrito();
+  //obtener el producto a agregar
+  const producto = productos.find(item => item.id == idProducto);
+  
+  // Verificar si el producto ya está en el carrito
+  let productoEnCarrito = carrito.productos.find(item => item.id == idProducto);
+  if (productoEnCarrito) {
+    // Si el producto ya está en el carrito, actualizar la cantidad
+    productoEnCarrito.cantidad += 1;
+  } 
+  else {
+    producto.cantidad = 1;
+    // Si el producto no está en el carrito, agregarlo con la cantidad
+    carrito.productos.push({
+      ...producto
+    });
+  }
+
+  // Actualizar el total y la cantidad del carrito
+  carrito.cantidad += 1;
+  carrito.total += producto.precio;
+
+  // Guardar el carrito en el localStorage
+  guardarCarrito(carrito);
+  actualizarCantidadCarrito();
+  mensajeAgregoAlCarrito();
+}
+
+function quitarAlCarrito(idProducto) {
+  let carrito = obtenerCarrito();
+  // Obtener el producto a eliminar
+  const productoBorrar = carrito.productos.find(item => item.id == idProducto);
+
+  if (productoBorrar) {
+    // Si el producto está en el carrito, eliminarlo
+    carrito.productos = carrito.productos.filter(item => item.id !== idProducto);
+
+    // Actualizar el total y la cantidad del carrito
+    carrito.cantidad -= productoBorrar.cantidad;
+    carrito.total -= productoBorrar.precio * productoBorrar.cantidad;
+
+    // Guardar el carrito en el localStorage
+    guardarCarrito(carrito);
+    location.reload();
+  }
+}
+
+function actualizarCantidadCarrito() {
+  const carrito = obtenerCarrito();
+  let cantidadCarrito = 0;
+  
+  carrito.productos.forEach(producto => {
+      cantidadCarrito += producto.cantidad;
+  });
+  
+  document.getElementById('cantidad-en-carrito').textContent = cantidadCarrito;
+}
+
+function vaciarCarrito() {
+  localStorage.removeItem('carrito');
+  location.reload();
+}
+
+function obtenerCarrito(){
+  let carritoGuardado = localStorage.getItem('carrito');
+  if(carritoGuardado){
+    return JSON.parse(localStorage.getItem('carrito'));
+  }
+  else{
+    return {
+      total: 0,
+      cantidad: 0,
+      productos: []
+    };
+  }
+}
+
+function guardarCarrito(carrito){
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+
+function mensajeAgregoAlCarrito()
+{
+  Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Se agrego correctamente el producto al carrito.',
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: false,
+      timer: 4000,
+      confirmButtonText:
+      '<a href="../html/carrito.html" style="text-decoration:none;color:white;">Ir al Carrito</a>',
+    confirmButtonAriaLabel: 'Ir al Carrito',
+    cancelButtonText:
+      'Seguir Comprando',
+    cancelButtonAriaLabel: 'Seguir Comprando'
+    })
 }
